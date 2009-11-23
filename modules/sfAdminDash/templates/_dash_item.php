21b60ae5e40865ff00bd2d6367557bfc67a060fd
Crafty_Shadow <Crafty_Shadow@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-11-23 10:40:02 +0000

    [sfAdminDashPlugin] Removed unused files

--- modules/sfAdminDash/templates/_dash_item.php
+++
@@ -1,6 +0,0 @@
-<div class="icon">
-  <a href="<?php echo url_for($item['url']) ?>">    
-    <?php echo image_tag($item['image'], array('alt' => $item['name'])) ?>
-    <span><?php echo $item['name'] ?></span>
-  </a>
-</div>
--- modules/sfAdminDash/templates/_item.php
+++
@@ -1,6 +0,0 @@
-<div class="icon">
-  <a href="<?php echo url_for($item['url']) ?>">
-    <img src="<?php echo $item['image'] ?>" />
-    <span><?php echo $item['name'] ?></span>
-  </a>
-</div>

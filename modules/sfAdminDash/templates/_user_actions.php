9a1d65af927aa45213c2d7b920b1332b1254dc63
Richtermeister <Richtermeister@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-05-06 18:22:44 +0000

    [sfAdminDashPlugin] moving "if($user_actions)" conditional into user actions partial, so that users can override user actions partial without having specified actions in yml

--- modules/sfAdminDash/templates/_user_actions.php
+++ modules/sfAdminDash/templates/_user_actions.php
@@ -1,5 +1,7 @@
+<?php if($user_actions): ?>
 <ul id="sf_admin_user_actions">
-  <?php foreach ($user_actions as $key =>$action): ?>
+  <?php foreach ($user_actions as $key => $action): ?>
   <li><?php echo link_to(__($key), $action['url']) ?></li>
   <?php endforeach; ?>
 </ul>
+<?php endif; ?>

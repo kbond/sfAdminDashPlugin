dfda643f5e25bc2ac75869b6e19a94d84e18e1c6
toaotc <enrico.thies@gmail.com>
2011-09-27 20:42:27 +0300

    Edited lib/sfAdminDash.class.php via GitHub

--- lib/sfAdminDash.class.php
+++ lib/sfAdminDash.class.php
@@ -66,7 +66,7 @@ class sfAdminDash
   public static function getItems()
   {
     $items = self::getProperty('items', array());
-    array_walk($items, array(self, 'initItem'));
+    array_walk($items, array(__CLASS__, 'initItem'));
 
     return $items;
   }
@@ -110,7 +110,7 @@ class sfAdminDash
     {
       if (isset($category_data['items']))
       {
-        array_walk($categories[$category_name]['items'], array(self, 'initItem'));
+        array_walk($categories[$category_name]['items'], array(__CLASS__, 'initItem'));
       }
     }
 

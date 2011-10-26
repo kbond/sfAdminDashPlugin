fd45615b4008dde0dc9a3e721bf25bf47ba4f4d7
CraftyShadow <vankata.t@gmail.com>
2011-10-26 22:47:05 +0300

    Update login form
    The login form now uses h2 for the "Login" label instead of an image,
    and will auto-focus the username field if both fields are empty,
    allowing for easier credentials entry

--- modules/sfAdminDash/templates/_login.php
+++ modules/sfAdminDash/templates/_login.php
@@ -1,17 +1,18 @@
-<?php 
+<?php
   use_helper('I18N');
-  use_stylesheet('/sfAdminDashPlugin/css/default.css'); 
+  use_stylesheet('/sfAdminDashPlugin/css/default.css');
 ?>
 
 <div id="ctr" align="center">
-  <div class="login">
+  <div class="login clear">
     <div class="login-form">
       <form action="<?php echo url_for(sfAdminDash::getProperty('login_route', '@sf_guard_signin')); ?>" method="post">
         <img alt="Login" src="<?php echo image_path(sfAdminDash::getProperty('web_dir', '/sfAdminDashPlugin').'/images/login.gif'); ?>" />
-        <div class="form-block">
+        <div class="form-block clear">
+		  <h2><?php echo __('Login panel', array(), 'sf_admin_dash'); ?></h2>
           <?php echo $form->renderGlobalErrors(); ?>
           <?php if(isset($form['_csrf_token'])): ?>
-            <?php echo $form['_csrf_token']->render(); ?> 
+            <?php echo $form['_csrf_token']->render(); ?>
 			    <?php endif; ?>
           <div class="inputlabel"><?php echo $form['username']->renderLabel(__('Username', array(), 'sf_admin_dash')); ?>:</div>
           <div>
@@ -27,7 +28,7 @@
             <?php echo $form['remember']->renderLabel(__('Remember?', array(), 'sf_admin_dash')); ?>
             <?php echo $form['remember']->render(array('class' => 'inputcheck')); ?>
           </div>
-          <div align="left"><input type="submit" name="submit" class="button clr" value="<?php echo __('Login', array(), 'sf_admin_dash'); ?>" /></div>
+          <div align="left"><input type="submit" name="submit" class="button clear" value="<?php echo __('Login', array(), 'sf_admin_dash'); ?>" /></div>
         </div>
       </form>
     </div>
@@ -36,7 +37,37 @@
       <p><?php echo __('Welcome to', array(), 'sf_admin_dash'); ?> <?php echo sfAdminDash::getProperty('site'); ?></p>
       <p><?php echo __('Use a valid username and password to gain access to the administration console.', array(), 'sf_admin_dash'); ?></p>
     </div>
-
-    <div class="clr"></div>
   </div>
 </div>
+
+
+<script type="text/javascript">
+(function (window, document) {
+
+    var autofocus_login = function() {
+        var is_focused = false,
+            elms = ['<?php echo $form['username']->renderId(); ?>',
+                    '<?php echo $form['password']->renderId(); ?>'];
+
+        for (var i=0; i<elms.length; i++) {
+            var elm = document.getElementById(elms[i]);
+            if (typeof elm !== 'undefined') {
+                if (elm.value != elm.defaultValue) {
+                     is_focused = true;
+                }
+            }
+        }
+
+        if (is_focused != true) {
+            document.getElementById(elms[0]).focus()
+        }
+    }
+
+    if (typeof jQuery !== 'undefined') {
+        jQuery(document).ready( autofocus_login );
+    } else {
+        window.onload = autofocus_login;
+    }
+
+}(this, this.document))
+</script>
